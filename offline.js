b06f08c10b09718a0b902d84942f5ed107823706
PlanKung <122417431+PlanKung@users.noreply.github.com>
2023-01-11 13:55:56 +0700

    Add files via upload

---
+++ offline.js
@@ -0,0 +1,23 @@
+﻿{
+	"version": 1673419159,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/bg-sheet0.png",
+		"images/bgbg-sheet0.png",
+		"images/ground-sheet0.png",
+		"images/player-sheet0.png",
+		"images/player-sheet1.png",
+		"images/l-sheet0.png",
+		"images/r-sheet0.png",
+		"images/coin-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
